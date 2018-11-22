afdb1ef361721d450f5782a3e9695dbebef72016
tunglam96hd <tunglam96hd@gmail.com>
2018-11-22 13:05:24 +0700

    sign up

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def full_title page_title = ""
+    page_title.empty? ? t(".base_title") : page_title + " | " + t(".base_title")
+  end
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -1,4 +1,13 @@
 class Member < ApplicationRecord
   has_many :follows
   has_many :stories, through: :follows
+
+  before_save {email.downcase!}
+  validates :name, presence: true, length: {maximum: Settings.Member.name.maximum}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: {maximum: Settings.Member.email.maximum},
+    format: {with: VALID_EMAIL_REGEX},
+    uniqueness: {case_sensitive: false}
+  validates :password, presence: true, length: {minimum: Settings.Member.password.minimum}
+  has_secure_password
 end
